50323053dccefe22cb69e8a8a0baee0dd355ecec
meticulixCEO <philosopherstand@gmail.com>
2026-04-19 13:55:43 +0300

    Updates

--- _next/static/aKEQc_cLhJevY35QBfkDJ/_buildManifest.js
+++ _next/static/aKEQc_cLhJevY35QBfkDJ/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/MeticulixIntelligenceLab//_next/:path+",
+        "destination": "/MeticulixIntelligenceLab/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
